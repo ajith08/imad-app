3d8c3e1c73fb994ef10fabc44a94f755544bbeb8
ajith08 <36699985+ajith08@users.noreply.github.com>
2018-03-04 22:55:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: '	ajithravikumar08',
+    database: '	ajithravikumar08',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -48,4 +57,17 @@ app.get('/submit', function(req, res){
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
+});
+
+var pool = Pool(config);
+app.get('/test', function(req, res) {
+    pool.query('SELECT * FROM test', function(err, result) {
+        if(err) {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
